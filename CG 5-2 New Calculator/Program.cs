f23192bb546f8f018ccae2f49b3363e8b76019ba
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-12 16:29:18 -0500

    Finished up calculations. Add comments

--- CG 5-2 New Calculator/Program.cs
+++ CG 5-2 New Calculator/Program.cs
@@ -12,14 +12,21 @@ namespace CG_5_2_New_Calculator
             Console.WriteLine("Welcome! Let's do some calculations, shall we?");
             Console.WriteLine("Please enter two numbers, each between 1 and 100.");
 
-            
+            /*This chunk of code converts inputed numbers the user provides and converts
+            them to integers that will be used in the calculations.*/
             int Input1 = int.Parse(Console.ReadLine());
             int Input2 = int.Parse(Console.ReadLine());
 
+            /*This line of code is asking the user to decide on what function the calculator
+            needs to perform on the integers*/
             Console.WriteLine("Please pick one of the following functions to perform:");
             Console.WriteLine("Add, Subtract, Multiply, Divide");
 
+            /*This variable is taking the inputed function from the user and making it 
+            accessible for the if statement that will be doing the calculations*/
             var operation = Console.ReadLine();
+
+            //if statements used to decide what function to use and then outputing that answer
 
             if (operation == "add")
             {
@@ -41,6 +48,10 @@ namespace CG_5_2_New_Calculator
                 Console.WriteLine(Divide(Input1, Input2));
             }
 
+            else
+            {
+                Console.WriteLine("Sorry.  I did not understand what you entered.")
+            }
 
             //Code written to make sure code worked prior to adding function methods
             //int z = Input1 + Input2;
@@ -56,8 +67,7 @@ namespace CG_5_2_New_Calculator
         /// <param name="input1- number 1-100"></param>
         /// <param name="input2 - number 1-100"></param>
         /// <returns>Adds input1 to input2 and outputs total
-        /// </returns>
-        
+        /// </returns>        
         public static int Addition(int input1, int input2)
         {
             return input1 + input2;
@@ -71,21 +81,35 @@ namespace CG_5_2_New_Calculator
         /// <param name="input2 - number 1-100"></param>
         /// <returns>Subtracts input1 from input2 and outputs the difference
         /// </returns>
-
         public static int Subtraction(int input1, int input2)
         {
             return input1 - input2;
         }
 
-
+        /// <summary>
+        /// This method is to multiply the user inputed integers
+        /// </summary>
+        /// <param name="input1 - number 1-100"></param>
+        /// <param name="input2 - number 1-100"></param>
+        /// <returns>Multiplies input1 to input2 and outputs the product.
+        /// </returns>        
         public static int Multiply(int input1, int input2)
         {
             return input1 * input2;
         }
 
-
-        public static int Divide(int input1, int input2)
+        /// <summary>
+        /// This method divides the user inputed integers
+        /// </summary>
+        /// <param name="input1 - number 1-100"></param>
+        /// <param name="input2 - number 1-100"></param>
+        /// <returns>Divides input1 by input2 and outputs the quotient.
+        /// </returns>
+        public static double Divide(double input1, double input2)
         {
+            //Code used to try and get decimals to show in answer - did not work
+            //var Answer = Convert.ToDouble(input1 / input2);
+            //return Answer;
             return input1 / input2;
         }
     }
